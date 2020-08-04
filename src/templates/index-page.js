ae9264a5533cb598e78baaa3c490add198cfd547
Neil Scheuermann <neilscheuermann@gmail.com>
2020-08-03 20:18:01 -0600

    Remove unused vars

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -5,6 +5,10 @@ import bethLindsTable from '../img/beth-linds-table.jpg'
 import bethLindsSaltLake from '../img/beth-linds-salt-lake.jpg'
 import {
   Section1Img,
+<<<<<<< HEAD
+=======
+  MainImage,
+>>>>>>> Remove unused vars
   Section1,
   Section1TextBox,
   Section1TextWrapper,
