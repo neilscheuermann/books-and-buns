d5b5e9534a360bd91c52a0640de95d5d1e47db08
Neil Scheuermann <neilscheuermann@gmail.com>
2020-08-09 20:03:55 -0600

    Trying different form example

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -47,18 +47,34 @@ export default class Index extends React.Component {
           }}
         >
           <MainText>Please email us at books.buns.co@gmail.com</MainText>
-          <form action="POST" data-netlify="true">
-            <input type="text" name="name" id="name" placeholder="Name" />
-            <input type="email" name="email" id="email" placeholder="Email" />
-            <textarea
-              id="message"
-              name="message"
-              cols="30"
-              rows="10"
-              placeholder="Message"
-            ></textarea>
-            <input type="file" name="myfile" id="myfile" />
-            <div data-netlify-recaptcha="true"></div>
+          <form
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="field half first">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field half">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" id="message" rows="6" />
+            </div>
+            <ul className="actions">
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Clear" />
+              </li>
+            </ul>
           </form>
         </div>
         {/* <section className="section"> */}
